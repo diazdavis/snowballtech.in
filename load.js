579506fd339ed663b37e7e058d3ec2a886e1f048
diazdavis <diazcddiaz@gmail.com>
2018-09-11 14:16:19 +0530

    offical webiste

---
+++ load.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#cover').remove();
+});
+
+$(window).load(function(){
+    $('body').fadeIn(5000);
+});
+
+$('img').center();
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
